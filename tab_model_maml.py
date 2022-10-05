4ca84d463d9268ca81bb31a3a70a59e41bf2df52
Ychanho <chanho0475@gmail.com>
2022-10-05 16:16:07 +0900

    tab_model_maml.py

--- tab_model_maml.py
+++ tab_model_maml.py
@@ -3,3 +3,43 @@ import numpy as np
 from pytorch_tabnet.utils import PredictDataset, filter_weights
 from pytorch_tabnet.abstract_model import TabModel
 from torch.utils.data import DataLoader
+
+
+class TabNetRegressor(TabModel):
+    def __post_init__(self):
+        super(TabNetRegressor, self).__post_init__()
+        self._task = 'regression'
+        self._default_loss = torch.nn.functional.mse_loss
+        self._default_metric = 'mse'
+
+    def prepare_target(self, y):
+        return y
+
+    def compute_loss(self, y_pred, y_true):
+        return self.loss_fn(y_pred, y_true)
+
+    def update_fit_params(
+        self,
+        X_train,
+        y_train,
+        eval_set,
+        weights
+    ):
+        if len(y_train.shape) != 2:
+            msg = "Targets should be 2D : (n_samples, n_regression) " + \
+                  f"but y_train.shape={y_train.shape} given.\n" + \
+                  "Use reshape(-1, 1) for single regression."
+            raise ValueError(msg)
+        self.output_dim = y_train.shape[1]
+        self.preds_mapper = None
+
+        self.updated_weights = weights
+        filter_weights(self.updated_weights)
+
+    def predict_func(self, outputs):
+        return outputs
+
+    def stack_batches(self, list_y_true, list_y_score):
+        y_true = np.vstack(list_y_true)
+        y_score = np.vstack(list_y_score)
+        return y_true, y_score
