06e77cff377e2e7251b2561b6ae07464027e97bb
Ychanho <chanho0475@gmail.com>
2022-10-05 20:13:53 +0900

    tab_model_maml.py

--- tab_model_maml.py
+++ tab_model_maml.py
@@ -19,7 +19,7 @@ from pytorch_tabnet.utils import (
 
 class TaNetRegressorMAML(TabNetRegressor):
     def __post_init__(self):
-        return super(TaNetRegressorMAML).__post_init__()
+        return super(TaNetRegressorMAML, self).__post_init__()
     # add maml variable maml=False
     def fit(
         self,
@@ -445,42 +445,3 @@ class TaNetRegressorMAML(TabNetRegressor):
             self.pin_memory,
         )
         return train_dataloader, valid_dataloaders
-
-# class TabNetRegressor(TabModel):
-#     def __post_init__(self):
-#         super(TabNetRegressor, self).__post_init__()
-#         self._task = 'regression'
-#         self._default_loss = torch.nn.functional.mse_loss
-#         self._default_metric = 'mse'
-
-#     def prepare_target(self, y):
-#         return y
-
-#     def compute_loss(self, y_pred, y_true):
-#         return self.loss_fn(y_pred, y_true)
-
-#     def update_fit_params(
-#         self,
-#         X_train,
-#         y_train,
-#         eval_set,
-#         weights
-#     ):
-#         if len(y_train.shape) != 2:
-#             msg = "Targets should be 2D : (n_samples, n_regression) " + \
-#                   f"but y_train.shape={y_train.shape} given.\n" + \
-#                   "Use reshape(-1, 1) for single regression."
-#             raise ValueError(msg)
-#         self.output_dim = y_train.shape[1]
-#         self.preds_mapper = None
-
-#         self.updated_weights = weights
-#         filter_weights(self.updated_weights)
-
-#     def predict_func(self, outputs):
-#         return outputs
-
-#     def stack_batches(self, list_y_true, list_y_score):
-#         y_true = np.vstack(list_y_true)
-#         y_score = np.vstack(list_y_score)
-#         return y_true, y_score
