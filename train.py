b46b72fff541acbe04d14ee2deee284cb2c94efc
Ychanho <chanho0475@gmail.com>
2022-10-19 17:37:05 +0900

    update wmaml train, valid function

--- train.py
+++ train.py
@@ -46,22 +46,62 @@ def valid(model, valid_loader):
 
 
 
-def wmaml_train():
-    pass
+def wmaml_train(maml, maml_dl_tr, maml_dl_val):
+    batch_num = len(maml_dl_tr[0])  
+    task_num = len(maml_dl_tr)
+    
+    maml.train()
+    tr_sampler = Sampler(dataloaders=maml_dl_tr)
+    val_sampler = Sampler(dataloaders=maml_dl_val)
+    # te_sampler = Sampler(dataloaders=maml_dl_te)
+    # print('-'*10,f'epoch: {epoch_idx}','-'*10)
+
+    for iter in range(batch_num):   #batch_num = len(maml_dl_tr[0]) / task_num = len(maml_dl_tr)
+        # print('-'*10,{iter},'-'*10)
+        optimizer.zero_grad()
+        meta_loss = 0.
+
+        # get batch datas
+        tr_samples = tr_sampler.get_sample()
+        val_samples = val_sampler.get_sample()
+        # te_samples = te_sampler.get_sample()
+
+        for task_idx in range(task_num):
+            task_model = maml.clone()
+
+            X_tr, y_tr = tr_samples[task_idx]
+            pred, M_loss = task_model(X_tr)
+            adaptation_loss = F.mse_loss(pred, y_tr)
+            task_model.adapt(adaptation_loss,
+                                allow_nograd=True,
+                                allow_unused=True)
+
+            X_val, y_val = val_samples[task_idx]
+            pred, M_loss = task_model(X_val)
+            evaluation_loss = F.mse_loss(pred, y_val)
+            meta_loss += evaluation_loss
+        meta_loss.backward()
+        optimizer.step()
+    # del(tr_sampler)
+    # del(val_sampler)
 
 def wmaml_valid(maml, maml_dl_te):
-    # check loss
+    ## Set phase
     maml.eval()
+
+    ## Valid start 
+    total_loss = 0.
+    outputs = torch.Tensor().cuda()
     with torch.no_grad():
-        total_loss = 0.
-        for i in range(len(maml_dl_te)):
+        for i in range(len(maml_dl_te)):    # check each task_meta_data
             total_task_loss = 0.
-            for data, target in maml_dl_te[i]:
+            for data, target in maml_dl_te[i]:  # iteration
                 output, M_loss = maml(data)
 
                 task_loss = F.mse_loss(output, target)
 
                 total_task_loss += task_loss.item()
+                outputs[i] = torch.cat((outputs[i], output))
             total_task_loss /= len(maml_dl_te[i])
 
             total_loss += total_task_loss
@@ -69,5 +109,5 @@ def wmaml_valid(maml, maml_dl_te):
 
         true = target.cpu().detach().numpy().squeeze()
         pred = output.cpu().detach().numpy().squeeze()
-        r2_res = r2_score(true, pred)
+        r2_res = r2_score(true, pred)   # r2_res의 result?
         return total_loss, outputs
