9fb32ec9983831568f019bb662de1c17922e7030
Ychanho <chanho0475@gmail.com>
2023-10-15 23:20:42 +0900

    update

--- tarin.py
+++ tarin.py
@@ -30,3 +30,21 @@ def train(model, train_loader, lr, loss_f='mse'):
         total_loss += loss.item()
     total_loss /= len(train_loader)
     return total_loss
+
+
+def valid(model, valid_loader, loss_f='mse'):
+    loss_f += '_loss'
+    ## Set phase
+    model.eval()
+    
+    ## Valid start    
+    total_loss = 0.
+    outputs = torch.Tensor().cuda()
+    with torch.no_grad():
+        for data, target in valid_loader:
+            output = model(data, target, train=False)
+            loss = getattr(F, loss_f)(output, target)
+            total_loss += loss.item()
+            outputs = torch.cat((outputs, output))
+    total_loss /= len(valid_loader)
+    return total_loss, outputs
