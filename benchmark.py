10b82ebfda44e3038cb3faa92b863b2b32f53345
Ychanho <chanho0475@gmail.com>
2023-10-16 03:54:19 +0900

    update

--- benchmark.py
+++ benchmark.py
@@ -2,4 +2,7 @@ import os
 from configs import *
 
 def exec_benchmark(recommend_command, opt):
-    return
+    os.system(f'sshpass -p {BENCHMARK_PW} ssh {BENCHMARK_ID}@{BENCHMARK_IP} "{recommend_command}"')
+    if os.path.exists('res.txt'):
+        os.system('rm res.txt')
+    os.system(f'sshpass -p {BENCHMARK_PW} ssh {BENCHMARK_ID}@{BENCHMARK_IP} "python3 show_ex.py" >> res.txt')
