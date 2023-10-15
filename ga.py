f77da0d73de05e167b95b00a215b139a837ae9e4
Ychanho <chanho0475@gmail.com>
2023-10-16 00:12:36 +0900

    update

--- ga.py
+++ ga.py
@@ -15,7 +15,7 @@ class RocksDBSingleProblem(Problem):
         self.model.eval()
         n_var = len(self.knobs.columns)
         n_obj = 1
-        self.ex_len = self.knobs.default_trg_em.shape[-1] # length of external metrics
+        self.ex_len = self.knobs.default_trg_em.shape[-1]
         xl = self.knobs.lower_boundary
         xu = self.knobs.upper_boundary
         
@@ -28,13 +28,9 @@ class RocksDBSingleProblem(Problem):
         scaled_def = self.knobs.scaler_y.transform(self.knobs.default_trg_em)
         
         outputs = self.single_score_function(scaled_def, outputs.cpu().detach().numpy().squeeze())
-        # outputs = self.single_score_function(self.knobs.default_trg_em, outputs.cpu().detach().numpy().squeeze())
         out["F"] = outputs
         
     def single_score_function(self, df, pr):
-        # df = np.repeat(df, pr.shape[0], axis=0)
         df = np.repeat(np.array(df), pr.shape[0], axis=0)
-        
-        
         score = (df[:,0] - pr[:,0]) + (pr[:,1] - df[:,1]) + (df[:,2] - pr[:,2]) + (df[:,3] - pr[:,3])
         return np.round(-score, 6)
