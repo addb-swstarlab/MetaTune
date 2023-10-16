35ed749b4dc38ee252a6772490b72db69391c569
Ychanho <chanho0475@gmail.com>
2023-10-16 16:44:37 +0900

    update

--- utils.py
+++ utils.py
@@ -18,5 +18,25 @@ def get_filename(PATH, head, tail):
         name = today+'/'+head+'-'+today+'-'+'%02d'%i+tail
     return name
     
-def get_logger():
-    return
+def get_logger(log_path='./logs'):
+
+    if not os.path.exists(log_path):
+        os.mkdir(log_path)
+
+    logger = logging.getLogger()
+    date_format = '%Y-%m-%d %H:%M:%S'
+    formatter = logging.Formatter('%(asctime)s[%(levelname)s] %(filename)s:%(lineno)s  %(message)s', date_format)
+    name = get_filename(log_path, 'log', '.log')
+    
+    fileHandler = logging.FileHandler(os.path.join(log_path, name))
+    streamHandler = logging.StreamHandler()
+    
+    fileHandler.setFormatter(formatter)
+    streamHandler.setFormatter(formatter)
+    
+    logger.addHandler(fileHandler)
+    logger.addHandler(streamHandler)
+    
+    logger.setLevel(logging.INFO)
+    logger.info('Writing logs at {}'.format(os.path.join(log_path, name)))
+    return logger, os.path.join(log_path, name)
