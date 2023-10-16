a2fb1a978b84a6c69e289cc2dfa849d1abd2c5d4
Ychanho <chanho0475@gmail.com>
2023-10-16 17:23:02 +0900

    update

--- configs.py
+++ configs.py
@@ -1,8 +1,8 @@
 # Variables relevant to benchmarking
 
-BENCHMARK_IP = ''
-BENCHMARK_ID = ''
-BENCHMARK_PW = ''
-BENCHMARK_TEXT_PATH = ''
-ROCKSDB_WORKLOAD_INFO = ''
-RESULT_STATISTICS = ''
+BENCHMARK_IP = 'insert server ip'   # ex) xxx.xxx.xxx.xx
+BENCHMARK_ID = 'insert user id or server'      
+BENCHMARK_PW = 'insert user password of server'   
+BENCHMARK_TEXT_PATH = 'insert path of saving the result of benchmark'   # ex) /home/admin/result.txt
+ROCKSDB_WORKLOAD_INFO = 'insert path of rocksdb_workload_info csv file' # ex) 'data/rocksdb/rocksdb_workload_info.csv'
+RESULT_STATISTICS = 'result_statistics.csv'
